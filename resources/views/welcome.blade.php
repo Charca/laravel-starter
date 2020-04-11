8ca16b24d60368ef224350ef98a594cc583ec2ac
Maxi Ferreira <charca@gmail.com>
2020-04-11 01:45:57 -0300

    Added App Shell and Blade Layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.default')
 
-        <title>Laravel</title>
-
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script defer src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
-        <div id="example"></div>
-    </body>
-</html>
+@section('content')
+    <div id="example"></div>
+@endsection
